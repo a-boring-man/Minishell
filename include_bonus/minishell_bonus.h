11a8a346c0315072c8e9c3c1e07867b1fbb8199d
Jonathan Rinna <jrinna@z4r11p1.42lyon.fr>
2022-03-22 15:50:16 +0100

    sujet bonus

--- include_bonus/minishell_bonus.h
+++ include_bonus/minishell_bonus.h
@@ -1,13 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.c                                        :+:      :+:    :+:   */
+/*   minishell_bonus.h                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jrinna <jrinna@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/22 14:02:25 by jrinna            #+#    #+#             */
-/*   Updated: 2022/03/22 15:48:54 by jrinna           ###   ########lyon.fr   */
+/*   Created: 2022/03/22 15:49:23 by jrinna            #+#    #+#             */
+/*   Updated: 2022/03/22 15:49:38 by jrinna           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#ifndef MINISHELL_BONUS_H
+# define MINISHELL_BONUS_H
+
+typedef struct s_minishell
+{
+	int	test;
+}	t_minishell;
+
+#endif
