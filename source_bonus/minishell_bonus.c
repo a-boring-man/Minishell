d235878b2149195cd4f6e5852d99eb450e81e07c
Jonathan Rinna <jrinna@z4r10p1.42lyon.fr>
2022-03-29 15:47:09 +0200

    compactage rapide et tentative pour ctrl + d

--- source_bonus/minishell_bonus.c
+++ source_bonus/minishell_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jrinna <jrinna@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 15:49:44 by jrinna            #+#    #+#             */
-/*   Updated: 2022/03/29 12:44:19 by jrinna           ###   ########lyon.fr   */
+/*   Updated: 2022/03/29 12:51:42 by jrinna           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,20 +24,17 @@ void	ft_ctrl_c(int i)
 void	ft_ctrl_backslash(int i)
 {
 	(void)i;
+	rl_replace_line("exit", 0);
+	rl_redisplay();
 	rl_on_new_line();
 	rl_redisplay();
 }
 
-int	main(void)
+void	ft_term_config(void)
 {
-	char			*test;
 	struct termios	old_config;
 	struct termios	new_config;
 
-	test = malloc(5);
-	test = "oups";
-	signal(SIGINT, ft_ctrl_c);
-	signal(SIGQUIT, ft_ctrl_backslash);
 	tcgetattr(STDIN_FILENO, &old_config);
 	new_config = old_config;
 	new_config.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP
@@ -45,6 +42,17 @@ int	main(void)
 	new_config.c_lflag &= ~(ECHOCTL | ECHONL | ICANON | ISIG | IEXTEN);
 	new_config.c_cflag &= ~(CSIZE | PARENB);
 	tcsetattr(STDIN_FILENO, 0, &new_config);
+}
+
+int	main(void)
+{
+	char			*test;
+
+	test = malloc(5);
+	test = "oups";
+	signal(SIGINT, ft_ctrl_c);
+	signal(SIGQUIT, ft_ctrl_backslash);
+	ft_term_config();
 	while (test)
 	{
 		test = readline("i'm depressed exit me $> ");
