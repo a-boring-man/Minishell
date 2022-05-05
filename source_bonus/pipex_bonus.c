5a4f065599f4ef914c44f6cf60f235a4d4cd6ba8
Julien Alamelle <jalamell@z4r11p5.42lyon.fr>
2022-05-05 15:57:57 +0200

    before merging jrinna

--- source_bonus/pipex_bonus.c
+++ source_bonus/pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jalamell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 16:05:36 by jalamell          #+#    #+#             */
-/*   Updated: 2022/05/03 17:10:40 by jalamell         ###   ########lyon.fr   */
+/*   Updated: 2022/05/05 15:54:59 by jalamell         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,53 @@ char	**ft_pipex_split(char *line)
 	char	**ret;
 }
 
+static void	child(char *cmd, int fd[3])
+{
+	char	**split;
+
+	split = split_token(cmd, fd);
+	//check in/out
+	//
+	//execve/moulinator
+}
+
 void	ft_pipex(t_minishell *mini, char *line)
 {/*
 	if (ft_is_a_built_in(line))
 		ft_call_built_in(mini, line);*/
+	char	**lst_cmd;
+	int		i;
+	int		fd[3];
+	int		pid;
 
-	//trim line
+	fd[0] = 0;
 	//split |
+	//lst_cmd = split(line, '|')
+	//
 	//foreach pipe fork
-	//check in/out
-	//execve/moulinator
+	if (!lst_cmd)
+		//return error
+	i = 0;
+	while (lst_cmd[i])
+	{
+		fd[2] = fd[0];
+		if (lst_cmd[i+1])
+			pipe(fd);
+		else
+		{
+			fd[1] = 1;
+			fd[0] = -1;
+		}
+		pid = fork();
+		if (!pid)
+			child(lst_cmd[i], fd);
+		else
+		{
+			close(fd[1]);
+			close(fd[2]);
+		}
+	}
+	while (wait(0) >= 0)
+		;
+	//return success
 }
