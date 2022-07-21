af56aa00db7edefaacf0a14ba1f69157f0cfc103
Jonathan Rinna <jrinna@z4r11p3.42lyon.fr>
2022-07-21 17:32:37 +0200

    encore plus de message de debug

--- source_bonus/utils_bonus.c
+++ source_bonus/utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jrinna <jrinna@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 09:28:03 by jrinna            #+#    #+#             */
-/*   Updated: 2022/07/21 15:31:42 by jrinna           ###   ########lyon.fr   */
+/*   Updated: 2022/07/21 17:01:30 by jrinna           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,8 +53,12 @@ void	ft_bt_ss(t_minishell *mini, char c, t_bt *gt, char **block_tmp)
 {
 	char	*tmp;
 
+ft_dprintf(2, "dans segmentor suit : block tmp -%s-\n", *block_tmp);
 	gt[mini->cb].next_operator_type = (c == '|');
+ft_dprintf(2, "dans segmentor suite : nextopp -%d-\n", gt[mini->cb].next_operator_type);
 	tmp = ft_strndup(*block_tmp, ft_strlen_s(*block_tmp) - 1);
+ft_dprintf(2, "dans segmentor suite : tmp -%s-\n", tmp);
 	gt[mini->cb++].lt = ft_tokenize_pipe(mini, tmp);
+ft_dprintf(2, "dans segmentor suite : after tokenizepipe-%p-\n", gt[mini->cb - 1].lt);
 	ft_free((void **)&tmp);
 }
