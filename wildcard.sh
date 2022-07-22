881b70114534dc3ce9fccf37b249d1d6fe519b64
Julien Alamelle <jalamell@z4r11p1.42lyon.fr>
2022-07-22 10:25:48 +0200

    cleaner

--- wildcard.sh
+++ wildcard.sh
@@ -1,7 +1,7 @@
-export FILE=`find source -name "*.c" | sed 's#.*/##g' | tr '\n' ' '`
-sed -i "" "s#LST_SRC :=.*#LST_SRC := $FILE#g" Makefile
-export FILE=`find include -name "*.h" | sed 's#.*/##g' | tr '\n' ' '`
-sed -i "" "s#LST_INC :=.*#LST_INC := $FILE#g" Makefile
+#export FILE=`find source -name "*.c" | sed 's#.*/##g' | tr '\n' ' '`
+#sed -i "" "s#LST_SRC :=.*#LST_SRC := $FILE#g" Makefile
+#export FILE=`find include -name "*.h" | sed 's#.*/##g' | tr '\n' ' '`
+#sed -i "" "s#LST_INC :=.*#LST_INC := $FILE#g" Makefile
 export FILE=`find source_bonus -name "*.c" | sed 's#.*/##g' | tr '\n' ' '`
 sed -i "" "s#LST_SRC_BNS :=.*#LST_SRC_BNS := $FILE#g" Makefile
 export FILE=`find include_bonus -name "*.h" | sed 's#.*/##g' | tr '\n' ' '`
